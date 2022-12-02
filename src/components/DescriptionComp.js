b50692e7e05f30e2e22ad13839b2ddb052bab2a2
Abhishek Jangid <70712283+AbhishekJangid20@users.noreply.github.com>
2022-12-02 11:52:00 +0530

    Add files via upload

---
+++ src/components/DescriptionComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DescriptionComp() {
+  return (
+    <div class="description">
+        <p>“The most welcomed, most enduring piece of daily mass e-mail in cyberspace.”</p>
+    </div>
+  );
+}
+
+export default DescriptionComp;
